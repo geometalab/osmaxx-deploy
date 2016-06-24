373e10373f51d0abd60399ffa5881b306dd7af46
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-06-24 12:17:59 +0200

    start exactly those osmaxx services which aren't already running

--- osmaxx/commands.py
+++ osmaxx/commands.py
@@ -1,5 +1,8 @@
+import os, re
+
 import root
-from command_definitions import Compose, container_exists, has_succeeded_or_is_running
+from ruamel import yaml
+from command_definitions import Compose, container_exists, is_running
 
 ENVIRONMENT = dict(
     COMPOSE_PROJECT_NAME='osmaxx2',
@@ -10,10 +13,9 @@ health_check = [
 ]
 
 pre_start = root.pre_start + [
-    Compose(['up', '-d', 'frontenddatabase']),
     Compose(
         ['run', '--rm', 'frontend', 'python3', 'web_frontend/manage.py', 'createsuperuser'],
-        predicate=lambda: not container_exists(r'[a-zA-Z0-9]+\_frontend\_[a-zA-Z0-9]+', name_is_regex=True)
+        predicate=lambda: not container_exists(container_name_regex('frontenddatabase'), name_is_regex=True)
     ),
 ]
 # Currently fails because of missing networks: https://github.com/docker/compose/issues/2908
@@ -22,15 +24,14 @@ pre_start = root.pre_start + [
 #     Compose(['create', '--no-recreate', '']),
 # ]
 
+with open(os.path.join(os.path.dirname(__file__), 'docker-compose.yml')) as f:
+    SERVICE_NAMES = yaml.load(f)['services'].keys()
+
 start = [
     Compose(
-        ['up', '-d'],
-        predicate=lambda: not container_exists('')
-    ),
-    Compose(
-        ['up', '-d'],
-        predicate=lambda: not has_succeeded_or_is_running('')
-    ),
+        ['up', '-d', service], predicate=lambda: not is_running(container_name_regex(service), name_is_regex=True)
+    )
+    for service in SERVICE_NAMES
 ]
 
 change = [
@@ -45,3 +46,10 @@ stop = [
 logs = [
     Compose(['logs']),
 ]
+
+
+def container_name_regex(service_name):
+    return r'{compose_project}\_{service}\_\d+'.format(
+        compose_project=re.escape(ENVIRONMENT['COMPOSE_PROJECT_NAME']),
+        service=re.escape(service_name),
+    )
